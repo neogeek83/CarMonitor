31c08a01b3034ee057e24192c963fce5e5789105
Kenny <NeoGeek83@gmail.com>
2016-03-07 19:57:04 -0500

    Expanding reading from 1 value to 6 seperate values

--- ImpedanceTester/ImpedanceTester.ino
+++ ImpedanceTester/ImpedanceTester.ino
@@ -1,26 +1,46 @@
-int analogPin= 0;
-int raw= 0;
+
+// Sensor reader
+int analogPins[6]= {0,1,2,3,4,5};
+int raw[6]= {0};
 int Vin= 5;
-float Vout= 0;
-float R1= 10000;
-float R2= 0;
-float buffer= 0;
+float Vout[6]= {0};
+float R1[6]= {10000,10000,10000,10000,10000,10000};
+float R2[6]= {0};
+float buffer[6]= {0};
+
+int timeInterval = 1000;//in msec
+int timeSinceStart = 0;
+
 
 void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  raw=analogRead(analogPin);
-  if(raw) {
-    buffer= raw * Vin;
-    Vout= (buffer)/1024.0;
-    buffer= (Vin/Vout) -1;
-    R2= R1 * buffer;
-    Serial.print("Vout: ");
-    Serial.println(Vout);
-    Serial.print("R2: ");
-    Serial.println(R2);
-    delay(1000);
+void readAnalogs(bool outputIt){
+  if (outputIt) Serial.print("Time ");Serial.print(timeSinceStart);
+
+  //Read in all 6 inputs, no way to read multiple at once, 
+  // so do it one at a time
+  for (int i=0;i<6;i++){
+    raw[i]=analogRead(analogPins[i]);
+    if(raw[i]) {
+      buffer[i]= raw[i] * Vin;
+      Vout[i]= (buffer[i])/1024.0;
+      buffer[i]= (Vin/Vout[i]) -1;
+      R2[i]= R1[i] * buffer[i];
+      if (outputIt){
+        Serial.print(" Vout[");Serial.print(i);Serial.print("] = ");
+        Serial.print(Vout[i]);
+        Serial.print(" R2[");Serial.print(i);Serial.print("] = ");
+        Serial.print(R2[i]);
+      }
+    } 
   }
+  if (outputIt) Serial.print("\n");
+}
+
+void loop() {
+  readAnalogs(true);
+  delay(timeInterval);
+  timeSinceStart++;
 }
